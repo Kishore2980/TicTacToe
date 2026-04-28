e793adc222617cc9da79898e851bea807815ba58
vn6743-star <vn6743@srmist.edu.in>
2026-04-28 15:47:50 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 import java.util.Scanner;
-
 public class Main {
 
     static char[][] board = {
@@ -20,26 +19,38 @@ public class Main {
         int col = getColFromSlot(slot);
 
         if (isValidMove(row, col)) {
-            System.out.println("Valid move at (" + row + ", " + col + ")");
+            placeMove(row, col, 'X');
+            System.out.println("Move placed!");
+
+            // print board
+            printBoard();
         } else {
             System.out.println("Invalid move!");
         }
     }
-
-    // UC4 → Slot to Row
     static int getRowFromSlot(int slot) {
         return (slot - 1) / 3;
     }
 
-    // UC4 → Slot to Column
     static int getColFromSlot(int slot) {
         return (slot - 1) % 3;
     }
-
-    // UC5 → Validation
     static boolean isValidMove(int row, int col) {
         return row >= 0 && row < 3 &&
                 col >= 0 && col < 3 &&
                 board[row][col] == '-';
+    }
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+
+    // extra (for display)
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
